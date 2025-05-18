f1510df415c8265c779685b9f37ed4989c16eebb
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-05-18 21:21:00 +0900

    Created: localStorage & sessionStorage
    1. persist middleware를 사용하여 제공
    2. sessionStorage는 해당 옵션을 추가해야 함
    3. 여러 장소를 만들 땐 각각 다른 name 지정

--- src/stores/useRoutineStore.js
+++ src/stores/useRoutineStore.js
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 const RoutineSchema = z.object({
   id: z.number(),
@@ -9,24 +10,33 @@ const RoutineSchema = z.object({
   // createdAt: z.string(),
 });
 
-const useRoutineStore = create((set) => ({
-  routines: [],
-  addRoutine: (routineData) => {
-    const routine = { ...routineData, id: Date.now() };
-    const result = RoutineSchema.safeParse(routine);
-    if (!result.success) {
-      // 유효하지 않으면 추가하지 않음
-      console.error("Invalid routine:", result.error.issues);
-      return;
+const useRoutineStore = create(
+  persist(
+    (set) => ({
+      routines: [],
+      addRoutine: (routineData) => {
+        const routine = { ...routineData, id: Date.now() };
+        const result = RoutineSchema.safeParse(routine);
+        if (!result.success) {
+          // 유효하지 않으면 추가하지 않음
+          console.error("Invalid routine:", result.error.issues);
+          return;
+        }
+        set((state) => ({
+          routines: [routine, ...state.routines],
+        }));
+      },
+      removeRoutine: (id) =>
+        set((state) => ({
+          routines: state.routines.filter((routine) => routine.id !== id),
+        })),
+    }),
+    {
+      // name: "fittracker-routines", //localStorage Key 이름
+      name: "fittracker-routines-session", //sessionStorage Key 이름
+      storage: createJSONStorage(() => sessionStorage), //session일때만
     }
-    set((state) => ({
-      routines: [routine, ...state.routines],
-    }));
-  },
-  removeRoutine: (id) =>
-    set((state) => ({
-      routines: state.routines.filter((routine) => routine.id !== id),
-    })),
-}));
+  )
+);
 
 export default useRoutineStore;

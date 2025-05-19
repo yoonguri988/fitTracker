d56c7eb9b925a59ca5b58d042c10e3d460f67595
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-05-19 22:29:17 +0900

    Updated: Add vaildate Function
    - 입력 검증 후 에러 메시지 표시

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,4 +1,4 @@
-function Input({ label, ...props }) {
+function Input({ label, errMsg, unitName, ...props }) {
   return (
     <div className="mb-4">
       {label && <label className="block mb-1 font-medium">{label}</label>}
@@ -6,6 +6,12 @@ function Input({ label, ...props }) {
         {...props}
         className="w-full px-3 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-green-400"
       />
+      {unitName && (
+        <span className="absolute right-5 top-7 transform -translate-y-5 text-base text-gray-400">
+          {unitName}
+        </span>
+      )}
+      {errMsg && <div className="my-2 text-red-400">{errMsg}</div>}
     </div>
   );
 }
